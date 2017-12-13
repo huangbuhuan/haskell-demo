de8318771ba631cc5c3e1a5b7fccc847b358c513
黄步欢 <buhuanhuang@gmail.com>
2017-12-13 23:39:01 +0800

    添加二叉搜索树

--- infix.hs
+++ infix.hs
@@ -1,3 +1,8 @@
 {- 只要以特殊字符命名函数，即可自动将它视为中缀函数。中缀值构造器必须以冒号开头 -}
 infixr 5 :-:
 data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 ^++
+(^++) :: List a -> List a -> List a
+Empty ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
