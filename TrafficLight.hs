2c59d787cc2a83dde258a676088edbfd28e09a3e
黄步欢 <buhuanhuang@gmail.com>
2017-12-14 22:07:19 +0800

    添加Show例子

--- TrafficLight.hs
+++ TrafficLight.hs
@@ -5,3 +5,8 @@ instance Eq TrafficLight where
     Green == Green = True
     Yellow == Yellow = True
     _ == _ = False
+
+instance Show TrafficLight where 
+    show Red = "Red light"
+    show Yellow = "Yellow light"
+    show Green = "Green light"
