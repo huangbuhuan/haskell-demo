15ae3ab13e3abe5acc1ae9c24fd1390886280725
黄步欢 <buhuanhuang@gmail.com>
2017-12-19 00:12:40 +0800

    添加绑定例子

--- helloworld.hs
+++ helloworld.hs
@@ -1,7 +1,16 @@
+import Data.Char
 -- main = putStr "hello, world"
 
-main = do
+{- main = do
     putStrLn "Hello, what's your name"
     -- 从输入读取一行并将结果绑定到name中，只能使用<- 不能使用=
     name <- getLine
-    putStrLn ("Hey " ++ name ++ ", you rock!")
+    putStrLn ("Hey " ++ name ++ ", you rock!") -}
+main = do
+    putStrLn "What's your first name?"
+    firstName <- getLine
+    putStrLn "What's your last name?"
+    lastName <- getLine
+    let bigFirstname = map toUpper firstName
+        bigLastName = map toUpper lastName
+    putStrLn $ "hey " ++ bigFirstname ++ " " ++ bigLastName ++ " how are you"
