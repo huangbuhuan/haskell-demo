1ee87539e9650d9ff17a123b706f224c30c70a2f
黄步欢 <buhuanhuang@gmail.com>
2017-12-18 23:04:37 +0800

    添加yesnoIf函数

--- TrafficLight.hs
+++ TrafficLight.hs
@@ -32,3 +32,9 @@ instance YesNo (Maybe a) where
 instance YesNo TrafficLight where
     yesno Red = False
     yesno _ = True
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult = 
+    if yesno yesnoVal
+        then yesResult
+        else noResult
