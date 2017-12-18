d7ca377f30bfa751c99e6ab3fe4162013528ff46
黄步欢 <buhuanhuang@gmail.com>
2017-12-18 23:00:34 +0800

    添加YesNo例子

--- TrafficLight.hs
+++ TrafficLight.hs
@@ -10,3 +10,25 @@ instance Show TrafficLight where
     show Red = "Red light"
     show Yellow = "Yellow light"
     show Green = "Green light"
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where 
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where 
+    yesno (Just _) = True
+    yesno Nothing = False
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
