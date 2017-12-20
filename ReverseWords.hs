9e4a307a6e8e4af9e08de75c427c4dc960f09d2b
黄步欢 <buhuanhuang@gmail.com>
2017-12-20 23:20:38 +0800

    添加反转字符串例子

---
+++ ReverseWords.hs
@@ -0,0 +1,13 @@
+module Main (main) where
+
+
+main :: IO ()
+main = do
+    line <- getLine
+    if null line
+        then return ()
+        else do
+            putStrLn $ reverseWords line
+            main
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words
